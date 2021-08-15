6054c42a18b0f8c2a8a899c79fd4dd2ca9bb7356
lindsaygelle <lindsay.gelle@gmail.com>
2021-08-16 00:32:43 +1000

    Added doc.go

---
+++ doc.go
@@ -0,0 +1,16 @@
+// Package nook exports basic information for characters that appear in the video game series Animal Crossing.
+//
+// Specifically this package focuses on the organisation and classification of residents and villagers.
+// Residents in the context of this package are Animal Crossing characters that cannot
+// be invited to live within the players town, city, village or island.
+// Villagers are Animal Crossing characters that live in homes within the players world but do not have
+// a special function, such as Tom Nook or Isabelle.
+//
+// This package separates Animal Crossing characters by their animal type. This is a requirement due to the number
+// of characters there are in the game series, and that some characters have overlaps in names with other villagers
+// or other elements that compose a character. An example of this is the character Petunia.
+//
+// The information for this package has largely been sourced from various Wikipedia packages across the internet.
+//
+// All contributions and corrections are welcome and encouraged.
+package nook
