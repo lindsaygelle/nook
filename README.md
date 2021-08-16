9192b4d8087ba96859c37ba4b9b672b2f9e4ab6a
lindsaygelle <lindsay.gelle@gmail.com>
2021-08-16 12:54:56 +1000

    Edited documentation

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # Nook
 Nook exports basic information for characters that appear in the video game series Animal Crossing.
 
-Specifically this package focuses on the organisation and classification of residents and villagers.
+Specifically this package centers on the organisation and classification of residents versus villagers, as well as
+defining the common attributes between them.
+
 Residents in the context of this package are Animal Crossing characters that cannot
-be invited to live within the players town, city, village or island.
+be invited as home owners within the players town, city, or island. Residents perform a special role within the world
+and often are quest givers. An example of a Resident would be Isabelle, Tom Nook or Guliver.
 
-Villagers are Animal Crossing characters that live in homes within the players world but do not have
-a special function, such as Tom Nook or Isabelle.
+Villagers are Animal Crossing characters that can be invited to stay in the players town, city or island,
+and make up the larger world population. Characters such as Alfonso, Tabby and Eggbert are all examples of Villagers. Villagers
+can be given gifts, have personalities and homes.
 
-This package separates Animal Crossing characters by their animal type. This is a requirement due to the number
-of characters there are in the series, and that some characters overlap with other villagers in name
-or other elements that compose a character. An example of this is the name Petunia, who is both a Cow and a Rhinoceros villager. 
-Another example is Snooty, which is the name of one of the wolves and a personality type.
+This package separates and organises Animal Crossing characters by their animal type. This is a requirement due to the large number
+of characters there are in the series, and that some characters overlap with other components in name. 
+An example of this is the name Petunia, who is both a Cow and a Rhinoceros. Another example is Snooty, which is the name of one of the wolves and a personality type.
 
 The information for this package has largely been sourced from various Wiki pages across the internet.
 
 All contributions and corrections are welcome and encouraged.
+
+## Go
+
+This package can be added as a dependency using Go's package manager.
+
+### Install
+Adding the package.
+
+```sh
+go get -u github.com/lindsaygelle/nook
+```
 
 ## Docker
 
--- doc.go
+++ doc.go
@@ -1,16 +1,21 @@
-// Package nook exports basic information for characters that appear in the video game series Animal Crossing.
+// Nook exports basic information for characters that appear in the video game series Animal Crossing.
 //
-// Specifically this package focuses on the organisation and classification of residents and villagers.
+// Specifically this package centers on the organisation and classification of residents versus villagers, as well as
+// defining the common attributes between them.
+//
 // Residents in the context of this package are Animal Crossing characters that cannot
-// be invited to live within the players town, city, village or island.
-// Villagers are Animal Crossing characters that live in homes within the players world but do not have
-// a special function, such as Tom Nook or Isabelle.
+// be invited as home owners within the players town, city, or island. Residents perform a special role within the world
+// and often are quest givers. An example of a Resident would be Isabelle, Tom Nook or Guliver.
 //
-// This package separates Animal Crossing characters by their animal type. This is a requirement due to the number
-// of characters there are in the game series, and that some characters have overlaps in names with other villagers
-// or other elements that compose a character. An example of this is the character Petunia.
+// Villagers are Animal Crossing characters that can be invited to stay in the players town, city or island,
+// and make up the larger world population. Characters such as Alfonso, Tabby and Eggbert are all examples of Villagers. Villagers
+// can be given gifts, have personalities and homes.
 //
-// The information for this package has largely been sourced from various Wikipedia packages across the internet.
+// This package separates and organises Animal Crossing characters by their animal type. This is a requirement due to the large number
+// of characters there are in the series, and that some characters overlap with other components in name.
+// An example of this is the name Petunia, who is both a Cow and a Rhinoceros. Another example is Snooty, which is the name of one of the wolves and a personality type.
+//
+// The information for this package has largely been sourced from various Wiki pages across the internet.
 //
 // All contributions and corrections are welcome and encouraged.
 package nook
