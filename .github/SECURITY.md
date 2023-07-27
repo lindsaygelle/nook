402d35eddac8cc01d5a24c6f98e42b659295ec42
lindsaygelle <lindsay.gelle@gmail.com>
2023-07-27 15:48:13 +1000

    Dev

---
+++ .github/SECURITY.md
@@ -0,0 +1,7 @@
+## Security
+
+If you discover a security issue, please bring it to my attention!
+
+### Reporting a Vulnerability
+
+If you have information about a security issue, or vulnerability in this package, kindly send your report.
--- doc.go
+++ doc.go
@@ -1,16 +1,8 @@
-// Package nook provides a comprehensive collection of character information from the beloved video game series, Animal Crossing.
+// Package nook provides a comprehensive collection of character information from the video game series, Animal Crossing.
 //
-// Overview:
-// - This package focuses on organizing and classifying characters into two main categories: residents and villagers.
-// - Residents are essential characters in the game world, playing unique roles and often serving as quest givers. They cannot be invited as homeowners to the player's town, city, or island. Examples of residents include Isabelle, Tom Nook, and Guliver.
-// - Villagers, on the other hand, form the larger world population and can be invited to stay in the player's town, city, or island. They possess distinct personalities, homes, and can receive gifts. Alfonso, Tabby, and Eggbert are among the charming villagers you may encounter.
+// This package focuses on organizing and classifying characters into two main categories: residents and villagers.
+// Residents are essential characters in the game world, playing unique roles and often serving as quest givers. They cannot be invited as homeowners to the player's town, city, or island. Examples of residents include Isabelle, Tom Nook, and Guliver.
+// Villagers, on the other hand, form the larger world population and can be invited to stay in the player's town, city, or island. They possess distinct personalities, homes, and can receive gifts. Alfonso, Tabby, and Eggbert are among the charming villagers you may encounter.
 //
-// Character Organization:
-// - To manage the vast number of characters in the series and potential naming overlaps, this package thoughtfully categorizes characters based on their animal type. For example, the name Petunia is shared by both a Cow and a Rhinoceros character, while Snooty is the name of both a wolf character and a personality type.
-//
-// Contribution:
-// - The information in this package has been diligently sourced from various reliable Wiki pages across the internet.
-// - We wholeheartedly welcome and encourage contributions and corrections from the Animal Crossing community to enhance and expand the character database.
-//
-// Embark on a delightful journey into the world of Animal Crossing with the wealth of character knowledge at your fingertips. Happy exploring!
+// To manage the vast number of characters in the series and potential naming overlaps, this package thoughtfully categorizes characters based on their animal type. For example, the name Petunia is shared by both a Cow and a Rhinoceros character, while Snooty is the name of both a wolf character and a personality type.
 package nook
